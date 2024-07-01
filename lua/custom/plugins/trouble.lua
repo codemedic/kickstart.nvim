d03a4a80dfa4a9446f9ab55ab4c2e398ac6a48d8
Dino Korah <codemedic@users.noreply.github.com>
2024-07-01 23:50:39 +0000

    add trouble options

--- lua/custom/plugins/trouble.lua
+++ lua/custom/plugins/trouble.lua
@@ -1,7 +1,42 @@
 -- https://github.com/folke/trouble.nvim
 return {
   'folke/trouble.nvim',
-  opts = {}, -- for default options, refer to the configuration section for custom setup.
+  opts = {
+    modes = {
+      -- Diagnostics Cascade
+      -- The following example shows how to create a new mode that shows only the most severe diagnostics.
+      -- Once those are resolved, less severe diagnostics will be shown.
+      cascade = {
+        auto_open = true,
+        mode = 'diagnostics', -- inherit from diagnostics mode
+        filter = function(items)
+          local severity = vim.diagnostic.severity.HINT
+          for _, item in ipairs(items) do
+            severity = math.min(severity, item.severity)
+          end
+          return vim.tbl_filter(function(item)
+            return item.severity == severity
+          end, items)
+        end,
+      },
+      -- Diagnostics for the current buffer and errors from the current project
+      curfile = {
+        mode = 'diagnostics', -- inherit from diagnostics mode
+        filter = {
+          any = {
+            buf = 0, -- current buffer
+            {
+              severity = vim.diagnostic.severity.ERROR, -- errors only
+              -- limit to files in the current project
+              function(item)
+                return item.filename:find((vim.loop or vim.uv).cwd(), 1, true)
+              end,
+            },
+          },
+        },
+      },
+    },
+  }, -- for default options, refer to the configuration section for custom setup.
   cmd = 'Trouble',
   keys = {
     {
