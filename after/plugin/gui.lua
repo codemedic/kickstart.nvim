5905b33158c5f3c9676045de060f1844f8fe6546
Dino Korah <691011+codemedic@users.noreply.github.com>
2026-04-04 04:51:14 +0100

    neovide: replace deprecated neovide_transparency with neovide_opacity

--- after/plugin/gui.lua
+++ after/plugin/gui.lua
@@ -15,7 +15,7 @@ if vim.g.neovide then
     return string.format('%x', math.floor((255 * vim.g.transparency) or 0.8))
   end
 
-  vim.g.neovide_transparency = vim.g.transparency
+  vim.g.neovide_opacity = vim.g.transparency
   vim.g.neovide_background_color = '#0f1117' .. alpha()
   vim.g.neovide_window_blurred = true
 
